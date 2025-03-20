9cfd4f571c83688de3b7809056ae6dc1fc2968d2
Hayk10002 <hayk.g.khachatryan@gmail.com>
2025-03-20 20:42:15 +0400

    Fix

--- main.cpp
+++ main.cpp
@@ -65,55 +65,74 @@ void naiveMatMul(MatrixView A, MatrixView B, MatrixView C)
     }
 }
 
-void recursiveMatMul(MatrixView A, MatrixView B, MatrixView C)
+void recursiveMatMulImpl(MatrixView A, MatrixView B, MatrixView C)
 {
-    if (A.row_count() == 1 && A.col_count() == 1 && B.row_count() == 1 && B.col_count() == 1)
+    if (A.col_count() != B.row_count() || B.col_count() != C.col_count() || A.row_count() != C.row_count()) // Invalid matrix dimensions
+        return;    
+
+    if (A.row_count() == 0 || A.col_count() == 0 || B.col_count() == 0) // Empty matrices
+        return;
+
+    int n = A.row_count(), m = A.col_count(), p = B.col_count();
+
+    if (n == m && m == p && p == 1) // base case
     {
         C(0, 0) += A(0, 0) * B(0, 0);
+        return;
     }
-    else
-    {
-        int row_mid = A.row_count() / 2;
-        int col_mid = A.col_count() / 2;
 
-        MatrixView A11 = A.getSubMatrix(0, row_mid, 0, col_mid);
-        MatrixView A12 = A.getSubMatrix(0, row_mid, col_mid, A.col_count());
-        MatrixView A21 = A.getSubMatrix(row_mid, A.row_count(), 0, col_mid);
-        MatrixView A22 = A.getSubMatrix(row_mid, A.row_count(), col_mid, A.col_count());
+    MatrixView A11 = A.getSubMatrix(0, n / 2, 0, m / 2);
+    MatrixView A12 = A.getSubMatrix(0, n / 2, m / 2, m);
+    MatrixView A21 = A.getSubMatrix(n / 2, n, 0, m / 2);
+    MatrixView A22 = A.getSubMatrix(n / 2, n, m / 2, m);
 
-        MatrixView B11 = B.getSubMatrix(0, row_mid, 0, col_mid);
-        MatrixView B12 = B.getSubMatrix(0, row_mid, col_mid, B.col_count());
-        MatrixView B21 = B.getSubMatrix(row_mid, B.row_count(), 0, col_mid);
-        MatrixView B22 = B.getSubMatrix(row_mid, B.row_count(), col_mid, B.col_count());
+    MatrixView B11 = B.getSubMatrix(0, m / 2, 0, p / 2);
+    MatrixView B12 = B.getSubMatrix(0, m / 2, p / 2, p);
+    MatrixView B21 = B.getSubMatrix(m / 2, m, 0, p / 2);
+    MatrixView B22 = B.getSubMatrix(m / 2, m, p / 2, p);
 
-        MatrixView C11 = C.getSubMatrix(0, row_mid, 0, col_mid);
-        MatrixView C12 = C.getSubMatrix(0, row_mid, col_mid, C.col_count());
-        MatrixView C21 = C.getSubMatrix(row_mid, C.row_count(), 0, col_mid);
-        MatrixView C22 = C.getSubMatrix(row_mid, C.row_count(), col_mid, C.col_count());
+    MatrixView C11 = C.getSubMatrix(0, n / 2, 0, p / 2);
+    MatrixView C12 = C.getSubMatrix(0, n / 2, p / 2, p);
+    MatrixView C21 = C.getSubMatrix(n / 2, n, 0, p / 2);
+    MatrixView C22 = C.getSubMatrix(n / 2, n, p / 2, p);
 
-        recursiveMatMul(A11, B11, C11);
-        recursiveMatMul(A12, B21, C11);
-        recursiveMatMul(A11, B12, C12);
-        recursiveMatMul(A12, B22, C12);
-        recursiveMatMul(A21, B11, C21);
-        recursiveMatMul(A22, B21, C21);
-        recursiveMatMul(A21, B12, C22);
-        recursiveMatMul(A22, B22, C22);
-    }
+    recursiveMatMulImpl(A11, B11, C11);
+    recursiveMatMulImpl(A12, B21, C11);
+    recursiveMatMulImpl(A11, B12, C12);
+    recursiveMatMulImpl(A12, B22, C12);
+    recursiveMatMulImpl(A21, B11, C21);
+    recursiveMatMulImpl(A22, B21, C21);
+    recursiveMatMulImpl(A21, B12, C22);
+    recursiveMatMulImpl(A22, B22, C22);
 }
 
+void recursiveMatMul(MatrixView A, MatrixView B, MatrixView C)
+{
+    for (int i = 0; i < C.row_count(); i++)
+        for (int j = 0; j < C.col_count(); j++)
+            C(i, j) = 0;
+    recursiveMatMulImpl(A, B, C);
+}
 
 int main()
 {
     std::vector<int> A = {1, 2, 3, 4, 5, 6};
     std::vector<int> B = {7, 8, 9, 10, 11, 12};
-    std::vector<int> C(4, 0);
+    std::vector<int> C(9, 0);
 
     MatrixView A_view(A, 2);
-    MatrixView B_view(B, 2);
-    MatrixView C_view(C, 2);
+    MatrixView B_view(B, 3);
+    MatrixView C_view(C, 3);
 
     naiveMatMul(A_view, B_view, C_view);
+
+    for (int i = 0; i < C.size(); i++)
+    {
+        std::cout << C[i] << " ";
+    }
+    std::cout << std::endl;
+
+    recursiveMatMul(A_view, B_view, C_view);
 
     for (int i = 0; i < C.size(); i++)
     {
